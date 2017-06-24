b2a58dfd21b7d9e05c39fa694dc389cc0b2010cf
pankaj kumar <pankajkumar.01869@gmail.com>
2017-06-24 20:09:13 +0530

    commit for rest client code

---
+++ src/main/java/com/pankaj/dev/Messenger/rest/client/RestClient.java
@@ -0,0 +1,46 @@
+package com.pankaj.dev.Messenger.rest.client;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RestClient {
+
+	public static void main(String[] args) {
+		 try {
+
+				URL url = new URL("http://localhost:8080/Messenger/webapi/messenger/");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : "
+							+ conn.getResponseCode());
+				}
+
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+
+				String output;
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					System.out.println(output);
+				}
+
+				conn.disconnect();
+
+			  } catch (MalformedURLException e) {
+
+				e.printStackTrace();
+
+			  } catch (IOException e) {
+
+				e.printStackTrace();
+
+			  }
+	}
+
+}
